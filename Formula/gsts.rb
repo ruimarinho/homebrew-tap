a6077fc5635b4a5897aaa05c8f2064bdd5c12530
Rui Marinho <ruipmarinho@gmail.com>
2020-11-01 22:53:45 +0000

    Add gsts formula

---
+++ Formula/gsts.rb
@@ -0,0 +1,22 @@
+require "language/node"
+
+class Gsts < Formula
+  desc "Obtain and store AWS STS credentials by authenticating via G Suite SAML"
+  homepage "https://github.com/ruimarinho/gsts"
+  url "https://github.com/ruimarinho/gsts/archive/v2.4.3.tar.gz"
+  sha256 "5df2f15aadb246c99ddc28186e5c33dbb0c15d138eeed5e93fd5dc1d1166c38b"
+  license "MIT"
+
+  bottle :unneeded
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gsts --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,21 @@
+# 🍻 Homebrew Tap
+
+A [Homebrew Tap](https://docs.brew.sh/Taps) for some of my open source projects.
+
+# Usage
+
+Tap this repository into to your local Homebrew installation:
+
+```shell
+❯ brew tap ruimarinho/tap
+```
+
+Install any available packages just like any other Homebrew package:
+
+```shell
+❯ brew install gsts
+```
+
+# License
+
+MIT
