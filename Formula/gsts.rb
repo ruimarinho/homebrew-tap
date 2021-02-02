65df2a6315ddf14210c2617739c2dc60a20abe00
Rui Marinho <ruipmarinho@gmail.com>
2021-02-02 12:35:11 +0000

    Update gsts@3.0.0

--- Formula/gsts.rb
+++ Formula/gsts.rb
@@ -3,13 +3,13 @@ require "language/node"
 class Gsts < Formula
   desc "Obtain and store AWS STS credentials by authenticating via G Suite SAML"
   homepage "https://github.com/ruimarinho/gsts"
-  url "https://github.com/ruimarinho/gsts/archive/v2.4.3.tar.gz"
-  sha256 "5df2f15aadb246c99ddc28186e5c33dbb0c15d138eeed5e93fd5dc1d1166c38b"
+  url "https://github.com/ruimarinho/gsts/archive/v3.0.0.tar.gz"
+  sha256 "1756ad6c3bfb86994ffc3dcc432e6a7166884f3dea51f8736bf25a9d7dddda1d"
   license "MIT"
 
   bottle :unneeded
 
-  depends_on "node@12"
+  depends_on "node"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
