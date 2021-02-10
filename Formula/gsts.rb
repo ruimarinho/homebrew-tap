4a1e1056870e909e0d9920e2c877ae58cd04d305
Rui Marinho <ruipmarinho@gmail.com>
2021-02-10 01:46:24 +0000

    Update gsts@3.0.1

--- Formula/gsts.rb
+++ Formula/gsts.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Gsts < Formula
   desc "Obtain and store AWS STS credentials by authenticating via G Suite SAML"
   homepage "https://github.com/ruimarinho/gsts"
-  url "https://github.com/ruimarinho/gsts/archive/v3.0.0.tar.gz"
-  sha256 "1756ad6c3bfb86994ffc3dcc432e6a7166884f3dea51f8736bf25a9d7dddda1d"
+  url "https://github.com/ruimarinho/gsts/archive/v3.0.1.tar.gz"
+  sha256 "6ec34ac1cc2bfd36cd55667fcfc405d795ba5c7e7fc48cb7873a7a7940f49f0e"
   license "MIT"
 
   bottle :unneeded
