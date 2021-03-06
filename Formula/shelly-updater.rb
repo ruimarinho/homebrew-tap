326c4688a47f541f95d7caafa9e071ae5e07b46a
goreleaserbot <ruipmarinho+goreleaserbot@gmail.com>
2021-03-06 15:33:38 +0000

    Brew formula update for shelly-updater version v1.1.0

--- Formula/shelly-updater.rb
+++ Formula/shelly-updater.rb
@@ -5,17 +5,17 @@
 class ShellyUpdater < Formula
   desc "A Shelly device firmware updater based on zeroconf (or bonjour) discovery for local networks using their built-in Over-The-Air update interface. It is suited for network setups where IoT devices do not have internet connectivity."
   homepage "https://github.com/ruimarinho/shelly-updater"
-  version "1.0.2"
+  version "1.1.0"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ruimarinho/shelly-updater/releases/download/v1.0.2/shelly-updater_1.0.2_macOS_x86_64.tar.gz"
-    sha256 "8ab407e0217c669a86403564682a8cd21e80b34eaff89762b1e158f561b801c9"
+    url "https://github.com/ruimarinho/shelly-updater/releases/download/v1.1.0/shelly-updater_1.1.0_macOS_x86_64.tar.gz"
+    sha256 "5a29be7e2ea3e1c551d1ec0b07da08efa2ded893eb4623533c2558aa891306c4"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ruimarinho/shelly-updater/releases/download/v1.0.2/shelly-updater_1.0.2_Linux_x86_64.tar.gz"
-    sha256 "fbe649ae5a43e01e877f0be60d31aa84886885f84e7270f5db65c66c34e4b6d8"
+    url "https://github.com/ruimarinho/shelly-updater/releases/download/v1.1.0/shelly-updater_1.1.0_Linux_x86_64.tar.gz"
+    sha256 "b9accdb394e7a0bb022b76e0c465916a5905601c22f5cbdbb9528600f33b766d"
   end
 
   def install
